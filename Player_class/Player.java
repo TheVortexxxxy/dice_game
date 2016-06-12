7d4816d2a0aa0cdc43d8b9aca67043a3e1cad14d
TheVortexxxxy <krzyspion2@gmail.com>
2016-06-12 16:47:12 +0200

    Update Player.java

--- Player_class/Player.java
+++ Player_class/Player.java
@@ -1,16 +1,43 @@
-package pio;
+package dicegame;
 
-abstract public class Player {
+/**
+ * Abstrakcyjna klasa bazowa dla klas graczy.
+ * 
+ * Definiuje abstrakcyjną metodę guess().
+ */
+
+public abstract class Player {
+
+    /**
+     * Pole nazwy (imienia) gracza.
+     * 
+     * Pole jest prywatne, więc dostarczamy setter i getter.
+     */
+    private String name = "anonymous";
+	
+	public Player() {}
+	
+	public Player(String name) {
+		setName(name);
+	}
     
-    private String name;
-
-    public void setName(String name) {
-        this.name = name;
+    public final void setName(String name) {
+		if (name != null && name.matches("^[a-zA-Z0-9_]{3,}$")) {				//"".equals(name)
+			this.name = name;
+		} else {
+			throw new IllegalArgumentException("Imie nie moze byc puste");
+		}
     }
-
+    
     public String getName() {
         return name;
     }
-
-    abstract public int guess();
+        
+    /**
+     * Metoda zwraca odgadniętą liczbę (strzał).
+     * 
+     * @return odgadnięta liczba 
+     */
+    public abstract int guess();
+    
 }
