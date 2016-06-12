02313dc83e70960069042410bae2fbed38810b49
TheVortexxxxy <krzyspion2@gmail.com>
2016-06-12 16:46:26 +0200

    Update pio.java

--- pio.java
+++ pio.java
@@ -1,32 +1,57 @@
-package pio;
+package dicegame;
 
 import java.util.Random;
 
-public class PIO {
+/**
+ * Gra w odgadywanie wylosowanej liczby.
+ * 
+ * Zasady:
+ * - komputer rzuca kostką (losuje liczby z zakresu 1..6)
+ * - gracz (komputer lub człowiek) stara się odgadnąć liczbę
+ * - jeżeli odgadnie, gra się kończy
+ * - jeżeli nie odgadnie, rozpoczyna się kolejna runda (komputer losuje kolejną liczbę i gracz stara się ją odgadnąć)
+ */
+public class DiceGame {
 
-    private static final Random random = new Random();
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         
-        Player ph = new PlayerComp();
-        ph.setName("mati");
-
-        int computer;
-        int player = 0;
-
+        /* Obiekt gracza trzymamy w zmiennej obiektowej typu Player.
+           Dzięki temu, że zastosowaliśmy dziedziczenie, możemy przypisać tej
+           zmiennej obiekt klasy PlayerComp lub PlayerHuman. Obiekty obu klas
+           mają metodę guess(), a to jest dla poniższego kodu najważniejsze.
+        */
+        
+        Player player = new PlayerComp();    //obiekt komputerowego gracza
+//        Player player = new PlayerHuman();   //obiekt gracza-człowieka
+        try {
+			player.setName("*");       //ustawiamy imię gracza za pomocą metody setName (czyli settera)
+		} catch(IllegalArgumentException ex) {
+			System.err.println("Blad!!!" +ex.getMessage());
+		}
+        Random dice = new Random();     //obiekt losujący (kostka)
+        int number,                     //wylosowana liczba
+            guess;                      //propozycja (strzał) gracza
+        
         do {
-            computer = random.nextInt(6) + 1;
-            //player = random.nextInt(6) + 1;
-            player = ph.guess();
+            number = dice.nextInt(6) + 1;
+            System.out.println("Wylosowane (number): " + number);
 
-            System.out.println("Wylosowano " + computer);
-            System.out.println("Gracz " +ph.getName()+ ": " + player);
+            guess = player.guess();     //wywołujemy metodę guess() obiektu gracza (klasy PlayerComp lub PlayerHuman)
 
-            if (computer == player) {
-                System.out.println("Brawo");
+            System.out.println("Gracz " + player.getName() + ": " + guess); //wyświetlamy informację zawierającą imię gracza i jego propozycję
+            
+            if (number == guess) {
+                System.out.println("BRAWO!");
             } else {
-                System.out.println("Żle");
+                System.out.println("PUDŁO!");
             }
-        } while (computer != player);
+            
+            System.out.println("---------------------------------------------");
+        
+        } while (number != guess);
     }
+    
 }
